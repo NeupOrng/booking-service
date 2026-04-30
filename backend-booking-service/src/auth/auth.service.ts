1196c8bc15b1ea1aa044f4af7e8c2b01c904ce26
God Orng <neuporng123@gmail.com>
2026-04-30 17:11:26 +0700

    fix vercel build

--- backend-booking-service/src/auth/auth.service.ts
+++ backend-booking-service/src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { ConflictException, Inject, Injectable, UnauthorizedException } from '@n
 import { REQUEST } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import * as crypto from 'crypto';
 import { Request } from 'express';
 import { eq, and } from 'drizzle-orm';
